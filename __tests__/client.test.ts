a939b2afa7a859cb4e809ca4f63870225a6dc6f9
Brian Giori <brian.giori@amplitude.com>
2023-11-08 15:35:25 -0800

    fix: fetch on start by default

--- __tests__/client.test.ts
+++ __tests__/client.test.ts
@@ -922,7 +922,7 @@ describe('start', () => {
     await client.start();
     expect(fetchSpy).toBeCalledTimes(1);
   }, 10000);
-  test('with local evaluation only, does not call fetch', async () => {
+  test('with local evaluation only, calls fetch', async () => {
     const client = new ExperimentClient(API_KEY, {});
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
@@ -931,7 +931,7 @@ describe('start', () => {
     };
     const fetchSpy = jest.spyOn(client, 'fetch');
     await client.start();
-    expect(fetchSpy).toBeCalledTimes(0);
+    expect(fetchSpy).toBeCalledTimes(1);
   });
 
   test('with local evaluation only, fetchOnStart enabled, calls fetch', async () => {
